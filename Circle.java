309c93c76c750f500fd01ade32f2bf1d4276a667
23NateK <n.kimber22@gmail.com>
2023-03-03 12:37:45 -0600

    Started on Circle

--- Circle.java
+++ Circle.java
@@ -1,3 +1,20 @@
 public class Circle {
-    
+    private Point2D point2d;
+    private double diam;
+public Circle(Point2D point2d, double diam){
+    this.point2d=point2d;
+    this.diam=diam;
+}
+
+
+
+
+    public static void main(String[] args) {
+        Point2D point2d = new Point2D(2.5, 1.0);
+        System.out.println(point2d.toString());
+        
+    }
+
+
+
 }
